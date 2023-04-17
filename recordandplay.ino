b9684169b94978ea4de4b350bcf395dd678ee20f
akash-shinde9 <121758341+akash-shinde9@users.noreply.github.com>
2023-04-17 12:39:35 +0530

    Update recordandplay.ino

--- recordandplay.ino
+++ recordandplay.ino
@@ -1,4 +1,8 @@
-
+/*
+   RFID SECURED GESTURE CONTROLLED ROBOTIC ARM WITH RECORD AND PLAYBACK FEATURE
+   Code by: Akash Shinde
+   Dated: 10-04-2023
+*/
 #include <Servo.h> //Servo header file
 
 //Declare object for 5 Servo Motors  
