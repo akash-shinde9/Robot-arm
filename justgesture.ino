186e57f123f36728d91bb53821197e30b221de19
akash-shinde9 <121758341+akash-shinde9@users.noreply.github.com>
2023-04-17 12:39:51 +0530

    Update justgesture.ino

--- justgesture.ino
+++ justgesture.ino
@@ -1,3 +1,8 @@
+/*
+   RFID SECURED GESTURE CONTROLLED ROBOTIC ARM WITH RECORD AND PLAYBACK FEATURE
+   Code by: Akash Shinde
+   Dated: 10-04-2023
+*/
 #include <Servo.h>  // add servo library
 
 Servo myservo1;  // create servo object to control a servo
